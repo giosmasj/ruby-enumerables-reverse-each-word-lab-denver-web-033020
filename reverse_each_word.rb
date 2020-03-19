0b178a67d109a1fe37a85b7adb3160897fa6b787
James Giosmas <giosmasj@gmail.com>
2020-03-19 04:06:28 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,19 @@
+def reverse_each_word_each(string)
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do |string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
+
+def reverse_each_ward_collect(string)
+  original_array = string.split(" ")
+  return_array = []
+  original_array.collect do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
+
+reverse_each_ward_collect("Hello there, and how are you?")
