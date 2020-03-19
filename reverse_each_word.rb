8d4d56dfab3da4f4866d968bb4e372eb78b4c125
James Giosmas <giosmasj@gmail.com>
2020-03-19 04:10:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,9 +10,7 @@ end
 def reverse_each_word_collect(string)
   original_array = string.split("")
   return_array = []
-  original_array.collect do|string|
-    return_array << string.reverse
-  end
+  return_array = original_array.collect {|word| word.reverse }
   return_array.join("")
 end
 
