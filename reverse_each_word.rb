51d6010889951a73c49218285e3a9247cff3fcc1
James Giosmas <giosmasj@gmail.com>
2020-03-19 04:16:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,13 @@
-def reverse_each_word_each(string)
-  original_array = string.split("")
+def reverse_each_word_each(sentence)
+  original_array = sentence.split(" ")
   return_array = []
-  return_array = original_array.each {|string| string.reverse }
-  return_array.join("")
-end
-
-def reverse_each_word_collect(string)
-  original_array = string.split(" ")
-  return_array = []
-  return_array = original_array.collect {|word| word.reverse }
+  return_array = original_array.each {|word| word.reverse }
   return_array.join(" ")
 end
 
-reverse_each_word_collect("Hello there, and how are you?")
+def reverse_each_word_collect(sentence)
+  original_array = sentence.split(" ").collect do |word|
+    word.reverse
+  end
+  original_array.join(" ")
+end
